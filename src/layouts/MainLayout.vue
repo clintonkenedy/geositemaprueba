6833cc32068757f93e538fb3e90ad8496d770246
clintonkenedy <clintonventuracondori@gmail.com>
2023-05-24 16:24:41 -0500

    footer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -101,9 +101,16 @@
 
     <q-page-container>
       <router-view />
-
     </q-page-container>
 
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
 
   </q-layout>
 </template>
